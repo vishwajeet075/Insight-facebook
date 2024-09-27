d01f1d1a508681b842face30482c8c9033187694
vishwajeet075 <shindevishwajeet434@gmail.com>
2024-09-27 21:53:07 +0530

    now final

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -38,7 +38,7 @@ export default function Navbar() {
               
               />
               <button className="btn btn-outline-success" type="submit" >
-                Sea job is d
+                Sea
               </button>
             </form>
 
